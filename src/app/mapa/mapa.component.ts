16be88b396603888cdd6c3a60179e799a2f91f9c
jorgeblancodeveloper <36778095+jorgeblancodeveloper@users.noreply.github.com>
2018-08-27 23:45:46 +0200

    Add files via upload

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -1,4 +1,3 @@
-
 import { Component, OnInit } from '@angular/core';
 //import { features } from ' ../../farmacias.json';
 
@@ -17,45 +16,79 @@ import { HttpErrorResponse } from '@angular/common/http';
 
 
 export class mapaComponent implements OnInit {
-//puntos = features;
-mostrar : boolean;
-values = [
-  { id: 1, name: "Todos" },
-  { id: 2, name: "Oeste" },
-  { id: 3, name: "Centro" },
-  { id: 4, name: "Este" }
-];
-filtro = "todos";
- public onChange(event): void {  // event will give you full breif of action
-    const newVal = event.target.value;
-    console.log(newVal);
-    this.filtro=newVal;
-  }
+    //puntos = features;
+    mostrar: boolean;
+    nombre = "";
+    telefono = "";
+    barrio = "";
+    values = [
+        { id: 1, name: "Barrio: Todos", value: "Todos" },
+        { id: 2, name: "Barrio: Oeste", value: "Oeste" },
+        { id: 3, name: "Barrio: Centro", value: "Centro" },
+        { id: 4, name: "Barrio: Este", value: "Este" }
+    ];
+    lat = 40.353861306107959;
+    lon = -3.534167475411653;
+    zoom = 12;
+    puntos = [];
+    filtro = "todos";
+    public onChange(event): void { // event will give you full breif of action
+        var  newVal = event.target.value;
+        this.filtro = newVal;
 
-clickedMarker(este){
-	console.log("clicked" + este);
-	this.mostrar = true;
-	this.nombre = this.farmacias.features[este].properties.NOMBRE;
-	this.telefono = this.farmacias.features[este].properties.TELEFONO;
-	this.barrio = this.farmacias.features[este].properties.BARRIO;
-}
-    constructor (private httpService: HttpClient) { }
-  farmacias: string [];
+        if (newVal != "Todos") {
+            this.puntos = this.farmacias.features.filter(item => item.properties.BARRIO == newVal);
+        }
+    }
+
+    public clickedMarker(este) {
+        if (this.farmacias) {
+        	this.lat =this.farmacias.features[este].geometry.coordinates[1];
+        	this.lon =this.farmacias.features[este].geometry.coordinates[0];
+        	this.zoom = 14;
+            this.mostrar = true;
+            this.nombre = this.farmacias.features[este].properties.NOMBRE;
+            this.telefono = this.farmacias.features[este].properties.TELEFONO;
+            this.barrio = this.farmacias.features[este].properties.BARRIO;
+
+        }
+    };
+    reinit() {
+    	console.log ("patata")
+    	 this.lat = 40.353861306107959;
+    this.lon = -3.534167475411653;
+    this.zoom = 12;
+    this.mostrar=false;
+    //  this.puntos = this.farmacias.features.filter(item => item.properties.BARRIO == this.filtro);
+    }
+
+    private icon = {
+        url: require('../../cruz.png'),
+        scaledSize: {
+            height: 30,
+            width: 30
+        }
+    };
+    constructor(private httpService: HttpClient) {}
+    farmacias: string[];
 
 
-    ngOnInit() { 
-    	 this.httpService.get('./assets/farmacias.json').subscribe(
-      data => {
-        this.farmacias = data as string [];	 // FILL THE ARRAY WITH DATA.
-          console.log(this);
-          
-      },
-      (err: HttpErrorResponse) => {
-        console.log (err.message);
-      }
-    );
+    ngOnInit() {
+        this.httpService.get('./assets/farmacias.json').subscribe(
+            data => {
+                this.farmacias = data as any[]; // FILL THE ARRAY WITH DATA.
+                //console.log(this.farmacias);
+                console.log(this.farmacias.features[3].properties.NOMBRE);
+                this.puntos = this.farmacias.features;
+
+            },
+            (err: HttpErrorResponse) => {
+                console.log(err.message);
+
+            }
+        );
+
 
     }
 
 }
-
